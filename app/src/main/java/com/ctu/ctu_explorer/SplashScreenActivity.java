56a69837aaee1add33b827a6ef224347f72bd88d
ngoan98tv <ngoan98travinh@gmail.com>
2019-08-08 11:52:08 +0700

    update splash screen text and initial building menu

--- app/src/main/java/com/ctu/ctu_explorer/SplashScreenActivity.java
+++ app/src/main/java/com/ctu/ctu_explorer/SplashScreenActivity.java
@@ -19,8 +19,15 @@ public class SplashScreenActivity extends AppCompatActivity {
                 .withSplashTimeOut(5000)
                 .withBackgroundColor(Color.parseColor("#ffffff"))
                 .withLogo(R.mipmap.ic_launcher)
+                .withHeaderText("A Product by CTU & DGIST Students")
                 .withAfterLogoText("CTU Explorer")
                 .withFooterText("Copyright 2019 by KIV Summer Project Team");
+
+        config.getHeaderTextView().setTextSize(10);
+        config.getHeaderTextView().setPadding(0,12,0,0);
+        config.getFooterTextView().setTextSize(10);
+        config.getFooterTextView().setPadding(0,0,0,12);
+
 
         View splashScreen = config.create();
         setContentView(splashScreen);
