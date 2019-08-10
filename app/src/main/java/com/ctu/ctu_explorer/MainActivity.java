23913fa17461b16864321db96fde389127a1450e
ngoan98tv <ngoan98travinh@gmail.com>
2019-08-10 12:13:21 +0700

    allow tap to set current location

--- app/src/main/java/com/ctu/ctu_explorer/MainActivity.java
+++ app/src/main/java/com/ctu/ctu_explorer/MainActivity.java
@@ -1,9 +1,8 @@
 package com.ctu.ctu_explorer;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
--- app/src/main/java/com/ctu/ctu_explorer/MapActivity.java
+++ app/src/main/java/com/ctu/ctu_explorer/MapActivity.java
@@ -4,27 +4,36 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.osmdroid.bonuspack.routing.OSRMRoadManager;
 import org.osmdroid.bonuspack.routing.Road;
 import org.osmdroid.bonuspack.routing.RoadManager;
 import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapEventsReceiver;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MapEventsOverlay;
 import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.Polyline;
 
@@ -35,11 +44,55 @@ public class MapActivity extends AppCompatActivity implements OnItemSelectedList
     private static final int MULTIPLE_PERMISSION_REQUEST_CODE = 4;
     private MapView mapView;
     private GeoPoint currentLocation;
-    private GeoPoint destination;
+    private Marker currentMarker;
+    private GeoPoint destinationPoint;
+    private Marker destinationMarker;
+    private Polyline routingOverlay;
+    private FusedLocationProviderClient fusedLocationClient;
+
+    final MapEventsReceiver mReceive = new MapEventsReceiver(){
+        @Override
+        public boolean singleTapConfirmedHelper(GeoPoint p) {
+            mapView.getOverlays().remove(destinationMarker);
+            mapView.getOverlays().remove(routingOverlay);
+            setCurrentLocation(p.getLatitude(),p.getLongitude());
+            return false;
+        }
+        @Override
+        public boolean longPressHelper(GeoPoint p) {
+            return false;
+        }
+    };
+
+    public void setCurrentLocation(double latitude, double longitude) {
+        mapView.getOverlays().remove(currentMarker);
+        currentLocation = new GeoPoint(latitude, longitude);
+        currentMarker = new Marker(mapView);
+        currentMarker.setPosition(currentLocation);
+        currentMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        currentMarker.setIcon(getResources().getDrawable(R.drawable.ic_person_pin_circle_black_24dp, getTheme()));
+        mapView.getOverlays().add(currentMarker);
+        mapView.getController().animateTo(currentLocation);
+        mapView.invalidate();
+    }
+
+    public void setDestination(double latitude, double longitude) {
+        mapView.getOverlays().remove(destinationMarker);
+        destinationPoint = new GeoPoint(latitude, longitude);
+        destinationMarker = new Marker(mapView);
+        destinationMarker.setPosition(destinationPoint);
+        destinationMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        destinationMarker.setIcon(getResources().getDrawable(R.drawable.ic_place_black_24dp, getTheme()));
+        mapView.getOverlays().add(destinationMarker);
+        mapView.getController().animateTo(destinationPoint);
+        mapView.invalidate();
+    }
 
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-        destination = Buildings.locations[pos];
-        getRouting();
+        if (Buildings.locations[pos] != null) {
+            setDestination(Buildings.locations[pos].getLatitude(), Buildings.locations[pos].getLongitude());
+            getRouting();
+        }
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
@@ -49,35 +102,37 @@ public class MapActivity extends AppCompatActivity implements OnItemSelectedList
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         // disable strict mode for easy development
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        //handle permissions first, before map is created. not depicted here
-
-        //load/initialize the osmdroid configuration, this can be done
         Context ctx = getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
-        //setting this before the layout is inflated is a good idea
-        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
-        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
-        //see also StorageUtils
-        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
 
-        //inflate and create the map
         setContentView(R.layout.activity_map);
 
-        checkPermissionsState();
+        checkPermissionsStateAndSetupMap();
+        getCurrentLocation();
+        mapView.getOverlays().add(new MapEventsOverlay(mReceive));
 
         Spinner buildingMenu = findViewById(R.id.building_selection);
         String[] buildings = Buildings.titles;
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, buildings);
         buildingMenu.setAdapter(adapter);
         buildingMenu.setOnItemSelectedListener(this);
+
+        ImageButton myLocationBtn = findViewById(R.id.my_location_btn);
+        myLocationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getCurrentLocation();
+            }
+        });
     }
 
-    private void checkPermissionsState() {
+    private void checkPermissionsStateAndSetupMap() {
         int internetPermissionCheck = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.INTERNET);
 
@@ -141,15 +196,18 @@ public class MapActivity extends AppCompatActivity implements OnItemSelectedList
     }
 
     private void setupMap() {
-
         mapView = findViewById(R.id.map_view);
+
         mapView.setClickable(true);
         mapView.setMultiTouchControls(true);
-        mapView.getController().setZoom(18.0);
+
         mapView.setTileSource(TileSourceFactory.MAPNIK);
 
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mapView.getController().setCenter(new GeoPoint(10.03088, 105.76813));
+        mapView.getController().setZoom(18.0);
+    }
 
+    public void getCurrentLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -161,49 +219,31 @@ public class MapActivity extends AppCompatActivity implements OnItemSelectedList
             return;
         }
 
-        Location mLastLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            setCurrentLocation(location.getLatitude(), location.getLongitude());
+                        }
+                    }
+                });
 
-        if (mLastLocation != null) {
-            double longitude = mLastLocation.getLongitude();
-            double latitude = mLastLocation.getLatitude();
-            currentLocation = new GeoPoint(latitude, longitude);
-        } else {
+        if (currentLocation == null) {
             Toast.makeText(this, "Please enable GPS to get your location", Toast.LENGTH_LONG).show();
-            currentLocation = new GeoPoint(10.03088, 105.76813);
         }
-
-        mapView.getController().setCenter(currentLocation);
-
-        Marker startMarker = new Marker(mapView);
-        startMarker.setPosition(currentLocation);
-        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        startMarker.setIcon(getResources().getDrawable(R.drawable.ic_person_pin_circle_black_24dp, getTheme()));
-        mapView.getOverlays().add(startMarker);
-
     }
 
     public void getRouting() {
-        if (currentLocation != null && destination != null) {
-            mapView.getOverlays().clear();
-
-            Marker startMarker = new Marker(mapView);
-            startMarker.setPosition(currentLocation);
-            startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-            startMarker.setIcon(getResources().getDrawable(R.drawable.ic_person_pin_circle_black_24dp, getTheme()));
-            mapView.getOverlays().add(startMarker);
-
+        if (currentLocation != null && destinationPoint != null) {
             ArrayList<GeoPoint> waypoints = new ArrayList<>();
+
             waypoints.add(currentLocation);
-
-            waypoints.add(destination);
-
-            Marker endMarker = new Marker(mapView);
-            endMarker.setPosition(destination);
-            endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-            endMarker.setIcon(getResources().getDrawable(R.drawable.ic_place_black_24dp, getTheme()));
-            mapView.getOverlays().add(endMarker);
+            waypoints.add(destinationPoint);
 
             RoadManager roadManager = new OSRMRoadManager(this);
+            ((OSRMRoadManager) roadManager).setService(getResources().getString(R.string.osrm_server_url));
+
             Road road = null;
             try {
                 road = roadManager.getRoad(waypoints);
@@ -211,8 +251,9 @@ public class MapActivity extends AppCompatActivity implements OnItemSelectedList
                 Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
             }
             if (road != null) {
-                Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
-                mapView.getOverlays().add(roadOverlay);
+                mapView.getOverlays().remove(routingOverlay);
+                routingOverlay = RoadManager.buildRoadOverlay(road);
+                mapView.getOverlays().add(routingOverlay);
                 mapView.invalidate();
             }
 
@@ -226,6 +267,7 @@ public class MapActivity extends AppCompatActivity implements OnItemSelectedList
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
         mapView.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+
     }
 
     public void onPause(){
