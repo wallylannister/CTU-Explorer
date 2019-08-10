1291ed82d3cf412c75714c076d501457ba3ebd03
ngoan98tv <ngoan98travinh@gmail.com>
2019-08-10 19:36:55 +0700

    update drawer menu

--- app/src/main/java/com/ctu/ctu_explorer/DrawerMenu.java
+++ app/src/main/java/com/ctu/ctu_explorer/DrawerMenu.java
@@ -6,19 +6,24 @@ import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 
 public class DrawerMenu {
     private static Drawer drawer;
 
     public static void create(Activity activity) {
-        drawer = new DrawerBuilder()
-                .withActivity(activity)
-                .addDrawerItems(
-                        new PrimaryDrawerItem().withIcon(R.drawable.ic_business_black_24dp),
-                        new DividerDrawerItem(),
-                        new PrimaryDrawerItem().withName("About")
-                )
-                .build();
+        DrawerBuilder drawerBuilder = new DrawerBuilder();
+        drawerBuilder.withActivity(activity);
+        drawerBuilder.addDrawerItems(
+                new PrimaryDrawerItem().withName("About").withIcon(R.drawable.ic_info_black_24dp),
+                new PrimaryDrawerItem().withName("Instructions").withIcon(R.drawable.ic_assignment_black_24dp),
+                new PrimaryDrawerItem().withName("Buildings Information").withIcon(R.drawable.ic_business_black_24dp),
+                new DividerDrawerItem(),
+                new PrimaryDrawerItem().withName("Report Problem").withIcon(R.drawable.ic_report_problem_black_24dp)
+        );
+        drawerBuilder.withSelectedItem(-1);
+        drawerBuilder.withHeader(R.layout.drawer_header);
+        drawer = drawerBuilder.build();
     }
 
     public static void close() {
